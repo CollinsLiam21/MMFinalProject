96fe76fbb017fadef6cd938f937b6eb188709f2a
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-12-22 22:55:40 -0500

    Updated from Brython Server: 12/22/2018 10:55:39 PM

--- Probabilitymap.py
+++ Probabilitymap.py
@@ -10,14 +10,14 @@ black = Color(0x000000,1)
 
 blackOutline = LineStyle(1,black)
 
-redRectangle = RectangleAsset(200,100,blackOutline,red) #width, height, outline, fill
+field = RectangleAsset(300,400,blackOutline,red) #width, height, outline, fill
 #blueCircle = CircleAsset(50,blackOutline,blue) #radius, outline, fill
 #greenEllipse = EllipseAsset(100,50,blackOutline,green) #width, height, outline, fill
 #blackLine = LineAsset(50,160,blackOutline) #x_endpoint, y_endpoint, lineStyle
 #redTriangle = PolygonAsset([(75,75), (100,100), (50,100)],blackOutline,red) #endpoint, outline, fill
 #text = TextAsset('Literature',fill=green, style='bold 40pt Times') #text, other options
 
-Sprite(redRectangle, (100,300))
+Sprite(field)
 #Sprite(blueCircle,(50,50))
 #Sprite(greenEllipse,(200,50))
 #Sprite(blackLine)
