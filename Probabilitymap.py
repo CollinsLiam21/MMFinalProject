f333f512fc6515f5bf81822c410c350e563546bb
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2019-01-10 13:46:08 -0500

    Updated from Brython Server: 1/10/2019 1:46:08 PM

--- Probabilitymap.py
+++ Probabilitymap.py
@@ -41,6 +41,11 @@ def mouseClick(event):
         Sprite(SoccerBall, (A-10,B-10))
         data['goalProb'] = '20%'
         Sprite(TextAsset(data['goalProb'],fill=black, style='bold 20pt Times'), (900,400))
+    #Location 7
+    if event.x < 250 and 225 < event.y < 300:
+        Sprite(SoccerBall, (A-10,B-10))
+        data['goalProb'] = '0%'
+        Sprite(TextAsset(data['goalProb'],fill=black, style='bold 20pt Times'), (900,400))
 
 red = Color(0xFF0000,1)
 green = Color(0x00FF00,1)
